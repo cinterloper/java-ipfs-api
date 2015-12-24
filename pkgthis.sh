e56289dc74f4de9b944deb1f8c48cad1d3902fa4
Grant Haywood <grant@iowntheinter.net>
2015-12-24 12:55:03 -0700

    Update pkgthis.sh

--- pkgthis.sh
+++ pkgthis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-    mvn deploy:deploy-file -DgroupId=net.iowntheinter \
-      -DartifactId=ipfs \
+    mvn deploy:deploy-file -DgroupId=org.ipfs \
+      -DartifactId=api \
       -Dversion=0.1 \
       -Dpackaging=jar \
       -Dfile=target/api-0.0.1-SNAPSHOT.jar \
-      -DrepositoryId=my-repo \
+      -DrepositoryId=repo \
       -Durl=file:///tmp/
